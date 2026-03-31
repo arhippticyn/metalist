90fc20d72d5778508fee38c32c708b0668b7f42e
arhippticyn <kakashihatake6hokagegygg@gmail.com>
2026-03-31 22:14:18 +0300

    fix bag #3

--- script.js
+++ script.js
@@ -2,106 +2,74 @@ const historyWrapper = document.querySelector('.history-wrapper')
 const factsWrapper = document.querySelector('.facts-wrapper')
 const playersWrapper = document.querySelector('.players-wrapper')
 
-fetch('./history.json')
-  .then(res => res.json())
-  .then(history => {
-    history.forEach(i => {
-      historyWrapper.innerHTML += `
-    <div class="swiper-slide">
+Promise.all([
+  fetch('./history.json').then(res => res.json()),
+  fetch('./facts.json').then(res => res.json()),
+  fetch('./players.json').then(res => res.json())
+])
+.then(([history, facts, players]) => {
+
+  history.forEach(i => {
+    historyWrapper.innerHTML += `
+      <div class="swiper-slide">
         <h2>${i.title}</h2>
         <p>${i.text}</p>
-        <img src=${i.image} width=500 height=300 alt="">
-
-    </div>
+        <img src="${i.image}" width="500" height="300" alt="${i.title}">
+      </div>
     `
-    })
-
-    const swiper = new Swiper('.swiper', {
-      loop: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      scrollbar: {
-        el: '.swiper-scrollbar',
-        draggable: true,
-      },
-      autoplay: {
-        delay: 5000,
-      },
-    })
   })
-  .catch(err => console.error('Помилка завантаження JSON:', err))
 
-fetch('./facts.json')
-  .then(res => res.json())
-  .then(facts => {
-    facts.forEach(i => {
-      factsWrapper.innerHTML += `
-        <div class="swiper-slide">
+  facts.forEach(i => {
+    factsWrapper.innerHTML += `
+      <div class="swiper-slide">
         <h2>${i.title} - <span>${i.value}</span></h2>
-        
         <p>${i.description}</p>
-        </div>
-        `
-    })
-
-    const swiper = new Swiper('.swiper', {
-      loop: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      scrollbar: {
-        el: '.swiper-scrollbar',
-        draggable: true,
-      },
-      autoplay: {
-        delay: 3000,
-      },
-    })
+      </div>
+    `
   })
-  .catch(err => console.error('Помилка завантаження JSON:', err))
 
-fetch('./players.json')
-  .then(res => res.json())
-  .then(facts => {
-    facts.forEach(i => {
-      playersWrapper.innerHTML += `
-<div class="swiper-slide player-card">
-  <img src="${i.image}" width=400 height=400 alt="${i.name}">
-  <h3>${i.name}</h3>
-  <p>${i.position}</p>
-  <span>${i.years}</span>
-</div>
-        `
-    })
-
-    const swiper = new Swiper('.swiper', {
-      loop: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      scrollbar: {
-        el: '.swiper-scrollbar',
-        draggable: true,
-      },
-      autoplay: {
-        delay: 3000,
-      },
-    })
+  players.forEach(i => {
+    playersWrapper.innerHTML += `
+      <div class="swiper-slide player-card">
+        <img src="${i.image}" width="400" height="400" alt="${i.name}">
+        <h3>${i.name}</h3>
+        <p>${i.position}</p>
+        <span>${i.years}</span>
+      </div>
+    `
   })
-  .catch(err => console.error('Помилка завантаження JSON:', err))
+
+
+  new Swiper('.history-swiper', {
+    loop: true,
+    autoplay: { delay: 5000 },
+    pagination: {
+      el: '.history .swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.history .swiper-button-next',
+      prevEl: '.history .swiper-button-prev',
+    }
+  })
+
+  new Swiper('.facts-swiper', {
+    loop: true,
+    autoplay: { delay: 3000 },
+    pagination: {
+      el: '.facts .swiper-pagination',
+      clickable: true,
+    }
+  })
+
+  new Swiper('.players-swiper', {
+    loop: true,
+    autoplay: { delay: 3000 },
+    pagination: {
+      el: '.players .swiper-pagination',
+      clickable: true,
+    }
+  })
+
+})
+.catch(err => console.error('Помилка:', err))
